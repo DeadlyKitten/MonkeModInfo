c70df72cbd1138bb6e5314c7ecd7a09894a4c137
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2021-10-02 17:21:05 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const https = require('https');
+const fs = require('fs');
+
+const prefix = 'https://api.github.com/repos/';
+const postfix = '/releases/latest';
+
+let inputJSON = fs.readFileSync('mods.json');
+let modList = JSON.parse(inputJSON);
+let result = []
+
+modList.mods.forEach(mod => 
+  https.get(`${prefix}${mod.gitPath}${postfix}`, { headers: { 'User-Agent' : 'DeadlyKitten/MonkeModInfo' ,'Authorization': `Token ${process.env.SECRET}`}},(res) => {
+    let body = "";
+
+      res.on("data", (chunk) => {
+          body += chunk;
+      });
+
+      res.on("end", () => {
+          try {
+            let json = JSON.parse(body);
+            result.push({
+              'name': mod.name,
+              'author': mod.author,
+              'version': json.tag_name,
+              'dependencies': mod.dependencies,
+              'dependents': mod.dependents,
+              'install_location': mod.installPath,
+              'group': mod.group,
+              'download_url': json.assets[mod.releaseId].browser_download_url
+            });
+          } catch (error) {
+              console.error(error.message);
+          };
+    });
+}));
+
+let attempts = 0;
+let timeout = 20;
+let interval = setInterval(() => {
+  if (result.length === modList.mods.length || attempts > timeout) {
+
+    result.sort(function(a,b) {
+      if (a.name === b.name) return 0;
+      return (a.name > b.name) ? 1 : -1;
+    });
+
+    fs.writeFileSync('modinfo.json', JSON.stringify(result, null, 2));
+
+    clearInterval(interval);
+  } else {
+    attempts++;
+  }
+}, 100);
